6ab878d27ac9199cf61d3cac7a1ea31648092a08
Tyler Pritchard <pritchard.tyler@gmail.com>
2021-08-30 15:41:54 -0700

    update day 2

--- day_2/README.md
+++ day_2/README.md
@@ -31,3 +31,4 @@ Functions
 [Microsoft](https://docs.microsoft.com/en-us/dotnet/visual-basic/programming-guide/language-features/procedures/differences-between-parameters-and-arguments)
 [Real Python](https://realpython.com/python-scope-legb-rule/#:~:text=%20Python%20Scope%20%26%20the%20LEGB%20Rule%3A%20Resolving,a%20special%20use%20case%20of%20the...%20More%20)
 [W3 Schools](https://www.w3schools.com/PYTHON/python_scope.asp)
+[Geeks for Geeks](https://www.geeksforgeeks.org/global-keyword-in-python/)
--- day_2/lecture.py
+++ day_2/lecture.py
@@ -54,3 +54,21 @@ def sadness():
 #joy(my_local_variable)
 
 #This is because my_local_variable is contained INSIDE the function sadness(), and is therefore not available to any functions outside of that SCOPE.
+
+#Global keyword is a keyword that allows a user to modify a variable outside of the current scope. It is used to create global variables from a non-global scope i.e inside a function. Global keyword is used inside a function only when we want to do assignments or when we want to change a variable. Global is not needed for printing and accessing.
+
+# Rules of global keyword:
+
+# If a variable is assigned a value anywhere within the function’s body, it’s assumed to be a local unless explicitly declared as global.
+# Variables that are only referenced inside a function are implicitly global.
+# We Use global keyword to use a global variable inside a function.
+# There is no need to use global keyword outside a function.
+
+x = 4
+
+def global_key():
+    global x
+    x = 444
+
+global_key()
+print(x)
